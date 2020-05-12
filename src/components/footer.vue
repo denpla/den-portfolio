f831245e15cf5492d296189e1105dd0bdf6c2783
Denis Plaksiychuk <denpla@gmail.com>
2020-05-12 10:56:38 +0200

    build

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer class="grey lighten-2">
     <v-spacer></v-spacer>
-    <div>denpla &copy; {{ new Date().getFullYear() }}</div>
+    <div>denpla &copy; {{ new Date().getFullYear() }} v1.0</div>
   </v-footer>
 </template>
 
